3e16e0475c8809d03c9c5b907ffa8621071f259a
ahamed.zoha@gmail.com <ahamed.zoha@gmail.com>
2020-01-03 21:47:50 +0600

    Added function template

--- serve.js
+++ serve.js
@@ -22,3 +22,24 @@ app.post('/dialogflow', express.json(), (req, res) => {
 })
 
 app.listen(process.env.PORT || 8080)
+
+// const TempQ = "temperatureQuery"
+
+// exports.dfFulfillment = functions.https.onRequest((req, res) => {
+
+//     const agent = new WebhookClient({
+//         request: req,
+//         response: res
+//     })
+//     console.log(`Dialogflow Request Headers: ${JSON.stringify(req.headers)}`)
+//     console.log(`Dialogflow Request Body: ${JSON.stringify(req.body)}`)
+
+//     function temperatureQ(agent) {
+//         console.log(`Intent cloudfunction matched ${req.body.parameters}`)
+//         agent.add(`Temperature Query Invoked!`)
+//     }
+
+//     let intentMap = new Map()
+//     intentMap.set("temperatureQuery", temperatureQ)
+//     agent.handleRequest(intentMap)
+// })
