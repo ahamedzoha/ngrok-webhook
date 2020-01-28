61be625dbbf2385223b6d78dd137dde1b8340b11
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-01-28 13:40:30 +0600

    removed auth

--- serve.js
+++ serve.js
@@ -1,3 +1,4 @@
+const firebase = require('firebase')
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyDXln2lxO_bUAN6ucd8xNd65ku0ubtxQ_4",
@@ -10,14 +11,8 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-
+const database = firebase.database()
 const express = require('express')
-const { WebhookClient } = ('dialogflow-fulfillment')
-
-//Access to firebase admin SDK for database read/write operation for fulfillment. TODO when launched as Cfunc
-// const admin = require('firebase-admin')
-// admin.initializeApp()
 
 // Import the appropriate class
 const { WebhookClient } = require('dialogflow-fulfillment')
@@ -26,41 +21,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('online'))
 
-//Need to refactor this line of the code for firebase cloud function
-app.post('/dialogflow', express.json(), (req, res) => {
-    const agent = new WebhookClient({ request: req, response: res })
 
-    function welcome() {
-        agent.add('Welcome to my agent')
-    }
-
-    let intentMap = new Map()
-    intentMap.set('Default Welcome Intent', welcome)
-    agent.handleRequest(intentMap)
-
-
-})
-
-app.listen(process.env.PORT || 8080)
-//
-
-// const TempQ = "temperatureQuery"
-
-// exports.dfFulfillment = functions.https.onRequest((req, res) => {
-
-//     const agent = new WebhookClient({
-//         request: req,
-//         response: res
-//     })
-//     console.log(`Dialogflow Request Headers: ${JSON.stringify(req.headers)}`)
-//     console.log(`Dialogflow Request Body: ${JSON.stringify(req.body)}`)
-
-//     function temperatureQ(agent) {
-//         console.log(`Intent cloudfunction matched ${req.body.parameters}`)
-//         agent.add(`Temperature Query Invoked!`)
-//     }
-
-//     let intentMap = new Map()
-//     intentMap.set("temperatureQuery", temperatureQ)
-//     agent.handleRequest(intentMap)
-// })
+const PORT= 8080 || process.env.PORT
+app.listen(PORT, () => `Server started at port ${PORT}`)
