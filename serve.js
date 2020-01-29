11154484ebe18e109d08a2547e825b6b3d6f44a3
ahamed.zoha@gmail.com <ahamed.zoha@gmail.com>
2020-01-29 14:35:21 +0600

    Firebase Database connection established and verified

--- serve.js
+++ serve.js
@@ -4,26 +4,42 @@ const firebase = require('firebase')
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 const database = firebase.database()
-
+const ref = database.ref('devices-telemetry')
 const express = require('express')
 const app = express()
 // Import the appropriate class
 const { WebhookClient } = require('dialogflow-fulfillment')
 
 //Start of the backend app
-app.get('/', (req, res) => res.send('online'))
+app.get('/', (req, res) => res.send('<h2>online</h2>'))
 
 app.post('/dialogflow', express.json(), (req, res) => {
 
     const agent = new WebhookClient({ request: req, response: res })
-    
+
+
+    let time = req.body.queryResult.parameters.time || req.body.queryResult.parameters.timep
+    let date = req.body.queryResult.parameters.date
+    let sensordata = req.body.queryResult.parameters.sensordata
+
     const welcome = () => {
         agent.add('Welcome to my agent!')
     }
 
     const tempQ = () => {
         agent.add('Temperature Query Invoked!')
-        console.log(JSON.stringify(req.body))
+        console.log(JSON.stringify(req.body.queryResult.parameters))
+
+
+        //USED FOR CONNECTION TESTING - CALLS THE ENTIRE DATABASE 
+        // ref.once('value', (data) => {
+        //     console.log(data.val())
+        // }, (err)=> console.log(err)
+        // )
+
+
+
+        if (time == 'last night') console.log('MATCH')
     }
 
     let intentMap = new Map()
@@ -35,5 +51,3 @@ app.post('/dialogflow', express.json(), (req, res) => {
 
 const PORT = 8080 || process.env.PORT
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
-
-// module.exports = router
