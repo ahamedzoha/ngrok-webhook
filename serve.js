e131137113b8e061f88d356f7ee688edf0fe466f
ahamed.zoha@gmail.com <ahamed.zoha@gmail.com>
2020-01-29 10:08:42 +0600

    Added post request

--- serve.js
+++ serve.js
@@ -2,17 +2,38 @@ const firebaseConfig = require('./firebase-auth')
 // Your web app's Firebase configuration
 const firebase = require('firebase')
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 const database = firebase.database()
-const express = require('express')
 
+const express = require('express')
+const app = express()
 // Import the appropriate class
 const { WebhookClient } = require('dialogflow-fulfillment')
 
-const app = express()
-
+//Start of the backend app
 app.get('/', (req, res) => res.send('online'))
 
+app.post('/dialogflow', express.json(), (req, res) => {
 
-const PORT= 8080 || process.env.PORT
-app.listen(PORT, () => `Server started at port ${PORT}`)
+    const agent = new WebhookClient({ request: req, response: res })
+    
+    const welcome = () => {
+        agent.add('Welcome to my agent!')
+    }
+
+    const tempQ = () => {
+        agent.add('Temperature Query Invoked!')
+        console.log(JSON.stringify(req.body))
+    }
+
+    let intentMap = new Map()
+    intentMap.set('Default Welcome Intent', welcome)
+    intentMap.set('temperatureQuery', tempQ)
+    agent.handleRequest(intentMap)
+})
+
+
+const PORT = 8080 || process.env.PORT
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
+
+// module.exports = router
